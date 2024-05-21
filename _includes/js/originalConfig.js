f12aa36cff784d55b1c9db038e2539929b29ce47
morelanneIF <168731309+morelanneIF@users.noreply.github.com>
2024-05-21 11:32:12 +0200

    Update originalConfig.js

--- _includes/js/originalConfig.js
+++ _includes/js/originalConfig.js
@@ -1,1 +1,47 @@
+var siteConfig = {
+  version: 2,
+  availableCatalogues: ['OCCURRENCE', 'DATASET', 'PUBLISHER', 'COLLECTION', 'INSTITUTION', 'LITERATURE'],
+  routes: {
+    enabledRoutes: ['occurrenceSearch', 'institutionKey', 'institutionSearch', 'publisherSearch', 'publisherKey', 'collectionKey', 'collectionSearch', 'datasetKey', 'datasetSearch', 'literatureSearch'],
+  },
+	@@ -23,20 +26,24 @@ var siteConfig = {
+      zoom: 1.8
+    },
+    rootPredicate: {
+      "type": "or",
+      "predicates": [
+        {
+          "key": "publishingCountry",
+          "type": "equals",
+          "value": countryCode
+        },
+        {
+          "type": "and",
+          "predicates": [
+            {
+              "key": "country",
+              "type": "equals",
+              "value": countryCode
+            },
+            {
+              "key": "notIssues",
+	@@ -48,7 +55,12 @@ var siteConfig = {
+      ]
+    },
+    highlightedFilters: ['taxonKey', 'gadmGid', 'stateProvince', 'publisherKey', 'elevation', 'year', 'basisOfRecord', 'datasetName', 'occurrenceIssue'],
+    // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
+    // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+  },
+  dataset: {
+	@@ -102,11 +114,37 @@ var siteConfig = {
+    highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
+  },
+  maps: {
+    locale: 'fr'
+  }
+};
 
+// example of a language specific route overwrite
+// if (pageLang === 'da')  {
+//   siteConfig.routes.occurrenceSearch.route = '/observationer/sog';
+// }
