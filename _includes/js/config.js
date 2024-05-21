54364f88c1fdeb6d7783c332055767e759668a19
morelanneIF <168731309+morelanneIF@users.noreply.github.com>
2024-05-21 11:53:24 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -10,7 +10,7 @@ if (primaryColor) {
   }});
 }
 
-var siteConfig = {   
+var siteConfig = {
   version: 2,
   availableCatalogues: ['OCCURRENCE', 'DATASET', 'PUBLISHER', 'COLLECTION', 'INSTITUTION', 'LITERATURE'],
   routes: {
@@ -20,27 +20,23 @@ var siteConfig = {
     mapSettings: {
       lat: 0,
       lng: 0,
-      zoom: 50
+      zoom: 1.8
     },
     rootPredicate: {
-      "type": "and",
+      "type": "or",
       "predicates": [
         {
+          "key": "publishingCountry",
           "type": "equals",
-	  "key": "country",
           "value": countryCode
         },
-	{
-          "type": "equals",
-	  "key": "networkKey",
-          "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba"
-	},
         {
-          "type": "not",
-          "predicates": {
-	      "type": "equals",
-              "key": "basisOfRecord",
-              "value": "PRESERVED_SPECIMEN"
+          "type": "and",
+          "predicates": [
+            {
+              "key": "country",
+              "type": "equals",
+              "value": countryCode
             },
             {
               "key": "notIssues",
@@ -52,12 +48,7 @@ var siteConfig = {
       ]
     },
     highlightedFilters: ['taxonKey', 'gadmGid', 'stateProvince', 'publisherKey', 'elevation', 'year', 'basisOfRecord', 'datasetName', 'occurrenceIssue'],
-    occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS', 'DASHBOARD'] // what tabs should be shown
-    availableTableColumns: ['scientificName', 'features', 'country', 'coordinates', 'eventDate', 'basisOfRecord', 'publisher', 'catalogNumber', 
-                            'recordedBy', 'identifiedBy', 'recordNumber', 'typeStatus', 'preparations', 'collectionCode', 'institutionCode', 'institutionKey', 
-                            'collectionKey', 'locality', 'higherGeography', 'stateProvince', 'year', 'establishmentMeans', 'iucnRedListCategory', 'dataset', 'datasetName'], // all the columns that are available to the user. This array defines the order they appear in.
-    defaultTableColumns: ['scientificName', 'higherGeography', 'country', 'year', 'establishmentMeans', 'iucnRedListCategory', 'catalogNumber', 'institutionKey', 'dataset', 'datasetName'] // the columns showed by default. The order is not relevant, as it is defined in the list of available columns. The user can change what columns to show in the UI.
-
+    // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
   },
   dataset: {
@@ -111,37 +102,6 @@ var siteConfig = {
     highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
   },
   maps: {
-    locale: 'de',
-    defaultProjection: 'MERCATOR', // what is the default projection
-    defaultMapStyle: 'BRIGHT', // what is the default style
-    mapStyles: {
-      MERCATOR: ['BRIGHT', 'NATURAL', 'SATELLITE', 'DARK'],
-    },
-    addMapStyles: function ({ mapStyleServer, language, pixelRatio, apiKeys, mapComponents }) {
-      return {
-        SWISSTOPO: { // the name of your style
-          component: mapComponents.OpenlayersMapbox,
-          labelKey: 'Swisstopo', // the label in the select. Use a translation key
-          mapConfig: {
-            basemapStyle: `https://vectortiles.geo.admin.ch/styles/ch.swisstopo.basemap.vt/style.json`,
-            projection: 'EPSG_3857'// one of 4326 | 3031 | 3857 | 3575
-          }
-        }
-      }
-    },
-    // rewire style names to show a different style
-    styleLookup: {
-      MERCATOR: {
-        BRIGHT: 'SWISSTOPO' 
-      }
-    }        
+    locale: 'fr'
   }
 };
-
-// example of a language specific route overwrite, in this example for showing the maps labels in the language of the site
-if (pageLang === 'fr')  {
-  siteConfig.maps.locale = 'fr';
-}
-if (pageLang === 'en')  {
-  siteConfig.maps.locale = 'en';
-}
