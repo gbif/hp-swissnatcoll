c647c8b3125432a01cce1dcfbb9571edbc4ea86a
morelanneIF <168731309+morelanneIF@users.noreply.github.com>
2024-06-10 15:16:16 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,15 +1,8 @@
-# ===========================================================================================
-# Variables and Initial Setup
-# ===========================================================================================
 var primaryColor = themeStyle && themeStyle.colors && themeStyle.colors.primary;
 var isSquared = themeStyle && themeStyle.square;
 
 const countryCode = 'CH';
-#const publisher = 'SVNHC';
 
-# ===========================================================================================
-# Conditional Theme Setup
-# ===========================================================================================
 if (primaryColor) {
   var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
     primary: primaryColor,
@@ -17,101 +10,113 @@ if (primaryColor) {
   }});
 }
 
-# ===========================================================================================
-# Configuration Object
-# ===========================================================================================
 var siteConfig = {
+  apiKeys: {
+   "mapbox": "pk.eyJ1IjoiaW5mb2ZhdW5hIiwiYSI6ImNsdzljY3JpODAxaXEycXBleGJsNTBqcHcifQ.DgU-N8lHtOSS0ogNiBnmow",
+ },  
   version: 2,
-  availableCatalogues: ['OCCURRENCE', 'DATASET', 'PUBLISHER', 'COLLECTION', 'INSTITUTION', 'LITERATURE'],
+  availableCatalogues: ['OCCURRENCE', 'LITERATURE', 'DATASET'],
   routes: {
-    enabledRoutes: ['occurrenceSearch', 'institutionKey', 'institutionSearch', 'publisherSearch', 'publisherKey', 'collectionKey', 'collectionSearch', 'datasetKey', 'datasetSearch', 'literatureSearch'],
+    enabledRoutes: ['occurrenceSearch', 'publisherKey', 'datasetKey', 'datasetSearch', 'literatureSearch'],
   },
-  occurrence: {			#OCCURRENCE_CONFIGURATION
+  occurrence: {
     mapSettings: {
-      lat: 0,
-      lng: 0,
-      zoom: 1.8
+      enabled: true,
+      lat: 46.801111,
+      lng: 8.626667,
+      zoom: 1000
     },
     rootPredicate: {
-      "type": "or",
+      "type": "and",
       "predicates": [
         {
-          "key": "publishingCountry",
           "type": "equals",
+          "key": "country",
           "value": countryCode
         },
         {
-          "type": "and",
-          "predicates": [
-            {
-              "key": "country",
-              "type": "equals",
-              "value": countryCode
-            },
-            {
-              "key": "notIssues",
-              "type": "equals",
-              "value": "COUNTRY_COORDINATE_MISMATCH"
-            }
-          ]
+          "type": "equals",
+          "key": "networkKey",
+          "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba"
+        },
+        {
+        "type": "not",
+        "predicate": {
+            "type": "equals",
+            "key": "basisOfRecord",
+            "value": "PRESERVED_SPECIMEN"
+          }
         }
       ]
     },
     highlightedFilters: ['taxonKey', 'gadmGid', 'stateProvince', 'publisherKey', 'elevation', 'year', 'basisOfRecord', 'datasetName', 'occurrenceIssue'],
-    // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
-    // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+    occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS', 'DASHBOARD'], // what tabs should be shown
+    availableTableColumns: ['scientificName', 'features', 'country', 'coordinates', 'eventDate', 'basisOfRecord', 'publisher', 'catalogNumber', 
+                            'recordedBy', 'identifiedBy', 'recordNumber', 'typeStatus', 'preparations', 'collectionCode', 'institutionCode', 'institutionKey', 
+                            'collectionKey', 'locality', 'higherGeography', 'stateProvince', 'year', 'establishmentMeans', 'iucnRedListCategory', 'dataset', 'datasetName'], // all the columns that are available to the user. This array defines the order they appear in.
+    defaultTableColumns: ['scientificName', 'higherGeography', 'country', 'year', 'establishmentMeans', 'iucnRedListCategory', 'catalogNumber', 'institutionKey', 'dataset', 'datasetName'] // the columns showed by default. The order is not relevant, as it is defined in the list of available columns. The user can change what columns to show in the UI.
+
   },
-  dataset: {			#DATASET_CONFIGURATION
+  dataset: {
     rootFilter: {publishingCountry: countryCode},
     highlightedFilters: ['q', 'anyPublisherKey', 'datasetType', 'license'],
     excludedFilters: ['publishingCountryCode'],
   },
-  publisher: {			#PUBLISHER_CONFIGURATION
+  publisher: {
     rootFilter: {country: countryCode},
     excludedFilters: ['countrySingle', 'networkKey'],
   },
-  collection: {			#COLLECTION_CONFIGURATION
-    excludedFilters: ['countryGrSciColl'],
-    rootFilter: {
-      displayOnNHCPortal: true,
-      country: countryCode,
-	  active: true
-    }
-  },
-  institution: {		#INSTITUTION_CONFIGURATION
-    excludedFilters: ['countryGrSciColl'],
-    rootFilter: {
-      displayOnNHCPortal: true,
-      country: countryCode,
-      active: true
-    },
-    mapSettings: {
-      enabled: true,
-      lat: 46.54547556985693,
-      lng: 8.092451942260254,
-      zoom: 7.877
-    },
-  },
-  literature: {			#LITERATURE_CONFIGURATION
+  literature: {
     rootFilter: {
       predicate: {
         type: 'or', predicates: [
           {
             type: 'in',
             key: 'countriesOfResearcher',
-            values: [countryCode]
+            values: countryCode
           },
           {
             type: 'in',
             key: 'countriesOfCoverage',
-            values: [countryCode]
+            values: countryCode
           }
         ]
       }
     },
     highlightedFilters: ['q', 'countriesOfResearcher', 'countriesOfCoverage', 'year']
   },
-  maps: {			#MAPS_CONFIGURATION
-    locale: 'fr'
+  maps: {
+    locale: 'en',
+    defaultProjection: 'MERCATOR', // what is the default projection
+    defaultMapStyle: 'NATURAL', // what is the default style
+    mapStyles: {
+      MERCATOR: ['BRIGHT', 'NATURAL', 'SATELLITE', 'DARK'],
+    },
+    addMapStyles: function ({ mapStyleServer, language, pixelRatio, apiKeys, mapComponents }) {
+      return {
+        SWISSTOPO: { // the name of your style
+          component: mapComponents.OpenlayersMapbox,
+          labelKey: 'Swisstopo', // the label in the select. Use a translation key
+          mapConfig: {
+            basemapStyle: `https://vectortiles.geo.admin.ch/styles/ch.swisstopo.basemap.vt/style.json`,
+            projection: 'EPSG_3857'// one of 4326 | 3031 | 3857 | 3575
+          }
+        }
+      }
+    },
+    // rewire style names to show a different style
+    styleLookup: {
+      MERCATOR: {
+        BRIGHT: 'SWISSTOPO' 
+      }
+    }        
   }
 };
+
+// example of a language specific route overwrite, in this example for showing the maps labels in the language of the site
+if (pageLang === 'fr')  {
+  siteConfig.maps.locale = 'fr';
+}
+if (pageLang === 'en')  {
+  siteConfig.maps.locale = 'en';
+}
