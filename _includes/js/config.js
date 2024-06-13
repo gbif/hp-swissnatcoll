1d446641a2819668e2ea8519275ef7d3055f2e3c
morelanneIF <168731309+morelanneIF@users.noreply.github.com>
2024-06-13 13:32:35 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -14,9 +14,9 @@ var siteConfig = {
    "mapbox": "pk.eyJ1IjoiaW5mb2ZhdW5hIiwiYSI6ImNsdzljY3JpODAxaXEycXBleGJsNTBqcHcifQ.DgU-N8lHtOSS0ogNiBnmow",
  },  
   version: 2,
-  availableCatalogues: ['OCCURRENCE', 'DATASET', 'INSTITUTION', 'LITERATURE'],
+  availableCatalogues: ['OCCURRENCE', 'COLLECTION', 'INSTITUTION', 'DATASET', 'LITERATURE'],
   routes: {
-    enabledRoutes: ['occurrenceSearch', 'institutionSearch', 'institutionKey', 'datasetKey', 'datasetSearch', 'literatureSearch'],
+    enabledRoutes: ['occurrenceSearch', 'institutionSearch', 'institutionKey', 'collectionSearch', 'collectionKey', 'datasetKey', 'datasetSearch', 'literatureSearch'],
   },
   occurrence: {
     mapSettings: {
