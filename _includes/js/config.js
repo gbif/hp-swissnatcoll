841c65094443a00e8687dc16ed25ef22ce646cc0
morelanneIF <168731309+morelanneIF@users.noreply.github.com>
2024-06-13 09:12:33 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -16,7 +16,7 @@ var siteConfig = {
   version: 2,
   availableCatalogues: ['OCCURRENCE', 'COLLECTION', 'INSTITUTION', 'TYPUS', 'DATASET', 'LITERATURE'],
   routes: {
-    enabledRoutes: ['occurrenceSearch', 'occurrenceKey', 'publisherSearch', 'publisherKey', 'datasetSearch', 'datasetKey', 'literatureSearch', 'litteratureKey', 
+    enabledRoutes: ['occurrenceSearch', 'occurrenceKey', 'publisherSearch', 'publisherKey', 'datasetsSearch', 'datasetsKey', 'literatureSearch', 'litteratureKey', 
 		    'collectionSearch', 'collectionKey', 'institutionSearch', 'institutionKey', 'typusSearch', 'typusKey'],
   },
   occurrence: {
@@ -31,13 +31,13 @@ var siteConfig = {
       "predicates": [
         {
           "type": "equals",
-          "key": "country",
+          "key": "publishing_country",
           "value": countryCode
         },
         {
           "type": "equals",
           "key": "networkKey",
-          "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba"
+          "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba" #Data network = SwissBIF (swissbif.ch)
         },
         {
           "type": "equals",
