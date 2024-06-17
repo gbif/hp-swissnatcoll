12c25a66d0feaa08efc9265705388af2aa85cf7f
morelanneIF <168731309+morelanneIF@users.noreply.github.com>
2024-06-17 13:44:46 +0200

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -27,15 +27,10 @@ var siteConfig = {
     rootPredicate: {
       "type": "and",
       "predicates": [
-        {
-          "type": "equals",
-          "key": "publishingCountry",
-          "value": countryCode
-        },
 	{
           "type": "equals",
-          "key": "networkKey",
-          "value": "0b00b924-016b-4954-96a7-2d9264b5d0ba" //Data network = SwissBIF (swissbif.ch)
+          "key": "publisherID",
+          "value": "64ee55c9-570a-42af-b7da-3f13c6b4e5a9"
         },
         {
           "type": "equals",
